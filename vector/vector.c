e05692d2afbba94d46ef43df93c6fcfca54f5ad7
Juan Barberis <juancebarberis@gmail.com>
2020-04-25 19:26:18 -0300

    vector initial

---
+++ vector/vector.c
@@ -0,0 +1,39 @@
+#include "vector.h"
+
+
+// Funciones del alumno.
+
+// ...
+
+
+// Funciones implementadas por la catedra.
+
+vector_t* vector_crear(size_t tam) {
+    vector_t* vector = malloc(sizeof(vector_t));
+
+    if (vector == NULL) {
+        return NULL;
+    }
+    vector->datos = malloc(tam * sizeof(int));
+
+    if (tam > 0 && vector->datos == NULL) {
+        free(vector);
+        return NULL;
+    }
+    vector->tam = tam;
+    return vector;
+}
+
+bool vector_redimensionar(vector_t* vector, size_t tam_nuevo) {
+    int* datos_nuevo = realloc(vector->datos, tam_nuevo * sizeof(int));
+
+    // Cuando tam_nuevo es 0, es correcto si se devuelve NULL.
+    // En toda otra situación significa que falló el realloc.
+    if (tam_nuevo > 0 && datos_nuevo == NULL) {
+        return false;
+    }
+
+    vector->datos = datos_nuevo;
+    vector->tam = tam_nuevo;
+    return true;
+}
