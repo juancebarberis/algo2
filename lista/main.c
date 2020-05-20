04f819afb7e2ef8aae5dd5915fa3df122b6b3843
Juan Barberis <juancebarberis@gmail.com>
2020-05-20 09:48:21 -0300

    lista initial

---
+++ lista/main.c
@@ -0,0 +1,22 @@
+#include "testing.h"
+#include <stdio.h>
+
+extern void pruebas_lista_alumno(void);
+extern void pruebas_lista_catedra(void);
+
+/* ******************************************************************
+ *                        PROGRAMA PRINCIPAL
+ * *****************************************************************/
+
+int main(void) {
+    /* Ejecuta todas las pruebas unitarias. */
+    printf("~~~ PRUEBAS DEL ALUMNO ~~~\n");
+    pruebas_lista_alumno();
+
+    #ifdef CORRECTOR
+        printf("\n~~~ PRUEBAS CÁTEDRA ~~~\n");
+        pruebas_lista_catedra();
+    #endif 
+
+    return failure_count() > 0;
+}
